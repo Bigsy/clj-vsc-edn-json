74cd386bda91ed154de5a1de0d0201b344bc82b8
Bill Hedworth <bill.hedworth@gmail.com>
2025-01-17 19:53:07 +0000

    jsontoedn

--- src/extension/core.cljs
+++ src/extension/core.cljs
@@ -1,21 +1,30 @@
 (ns extension.core
-  (:require ["vscode" :as vscode]))
+  (:require ["vscode" :as vscode]
+            [cljs.tools.reader.edn :as edn]
+            [clojure.pprint :as pp]))
 
-(defn hello-world
+(defn json->edn
   []
   (let [editor (.. vscode -window -activeTextEditor)]
     (when editor
-      (let [position (.-selection editor)]
-        (-> (.edit editor
-                  (fn [edit]
-                    (.insert edit (.-active position) "Hello World!")))
-            (.then #(println "Text inserted")))))))
+      (let [document (.-document editor)
+            selection (.-selection editor)
+            selected-text (.getText document selection)]
+        (try
+          (let [json-data (js->clj (js/JSON.parse selected-text) :keywordize-keys true)
+                edn-str (with-out-str (pp/pprint json-data))]
+            (-> (.edit editor
+                      (fn [edit]
+                        (.replace edit selection edn-str)))
+                (.then #(println "JSON converted to EDN successfully"))))
+          (catch :default e
+            (.. vscode -window (showErrorMessage (str "Error converting JSON: " (.-message e))))))))))
 
 (defn activate
   [context]
   (let [disposable (.. vscode -commands
                       (registerCommand
-                       "cljs-hello-world.helloWorld"
-                       hello-world))]
+                       "clj-vsc-formatter.jsonToEdn"
+                       json->edn))]
     (.push (.-subscriptions context) disposable)
-    (println "ClojureScript extension activated!")))
+    (println "ClojureScript JSON to EDN converter activated!")))
